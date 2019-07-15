467b50ac2ccc6e35209de232bbb3fbcae805b125
TSUYUSATO Kitsune <make.just.on@gmail.com>
2019-07-16 04:41:35 +0900

    feat: add ContT

---
+++ modules/nyan/src/main/scala/codes/quine/labo/nyan/data/ContT.scala
@@ -0,0 +1,13 @@
+package codes.quine.labo.nyan
+package data
+
+final case class ContT[R, M[_], A](run: (A => M[R]) => M[R])
+
+object ContT {
+  implicit def ContTInstances[R, M[_]](implicit M: Monad[M]): Monad[ContT[R, M, ?]] = new Monad[ContT[R, M, ?]] {
+    def pure[A](a: A): ContT[R, M, A] = ContT(k => k(a))
+
+    def flatMap[A, B](fa: ContT[R, M, A])(f: A => ContT[R, M, B]): ContT[R, M, B] =
+      ContT(k => fa.run(a => f(a).run(k)))
+  }
+}
