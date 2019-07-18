f3723d665caf44f532d06d5d58ac5f48ee1973ad
TSUYUSATO Kitsune <make.just.on@gmail.com>
2019-07-19 00:00:48 +0900

    fix: make ContT Monad instance stack safe

--- modules/neko/src/main/scala/codes/quine/labo/neko/data/ContT.scala
+++ modules/neko/src/main/scala/codes/quine/labo/neko/data/ContT.scala
@@ -4,10 +4,18 @@ package data
 final case class ContT[R, M[_], A](run: (A => M[R]) => M[R])
 
 object ContT {
-  implicit def ContTInstances[R, M[_]: Monad]: Monad[ContT[R, M, ?]] = new Monad[ContT[R, M, ?]] {
+  implicit def ContTInstances[R, M[_]: Monad: Defer]: Monad[ContT[R, M, ?]] = new Monad[ContT[R, M, ?]] {
     def pure[A](a: A): ContT[R, M, A] = ContT(k => k(a))
 
     def flatMap[A, B](fa: ContT[R, M, A])(f: A => ContT[R, M, B]): ContT[R, M, B] =
       ContT(k => fa.run(a => f(a).run(k)))
+
+    override def tailRecM[A, B](a: A)(f: A => ContT[R, M, Either[A, B]]): ContT[R, M, B] =
+      ContT { k =>
+        f(a).run {
+          case Left(a)  => Defer[M].defer(tailRecM(a)(f).run(k))
+          case Right(b) => k(b)
+        }
+      }
   }
 }
