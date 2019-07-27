f5731dc93f2b47b56e02f7254d3e5a7419631f74
TSUYUSATO Kitsune <make.just.on@gmail.com>
2019-07-27 11:49:51 +0900

    fix: refactor Eval

--- modules/neko/src/main/scala/codes/quine/labo/neko/data/Eval.scala
+++ modules/neko/src/main/scala/codes/quine/labo/neko/data/Eval.scala
@@ -3,171 +3,163 @@ package data
 
 import scala.annotation.tailrec
 
-sealed trait Eval[+A] { self =>
+trait Eval[A] {
   import Eval._
 
   def value: A
 
   def memoize: Eval[A]
 
-  def map[B](f: A => B): Eval[B] =
-    flatMap(x => Now(f(x)))
+  def map[B](f: A => B): Eval[B] = flatMap(a => Now(f(a)))
 
   def flatMap[B](f: A => Eval[B]): Eval[B] =
-    self match {
-      case c: FlatMap[A] =>
-        new FlatMap[B] {
-          type S = c.S
-          val start: () => Eval[c.S] = c.start
-          val run: c.S => Eval[B] = s =>
-            new FlatMap[B] {
-              type S = A
-              val start: () => Eval[A] = () => c.run(s)
-              val run: A => Eval[B] = f
-            }
-        }
-      case c: Defer[A] =>
-        new FlatMap[B] {
-          type S = A
-          val start: () => Eval[A] = c.thunk
-          val run: A => Eval[B] = f
-        }
-      case _ =>
-        new FlatMap[B] {
-          type S = A
-          val start: () => Eval[A] = () => self
-          val run: A => Eval[B] = f
-        }
+    this match {
+      case FlatMap(fa, f1) => FlatMap(fa, f1 :+ f)
+      case _               => FlatMap(this, AndThen.lift(f))
     }
 }
 
 object Eval {
   def now[A](a: A): Eval[A] = Now(a)
+  def always[A](a: => A): Eval[A] = Always(a _)
+  def later[A](a: => A): Eval[A] = Later(a _)
+  def defer[A](fa: => Eval[A]): Eval[A] = Defer(fa _)
 
-  def later[A](a: => A): Eval[A] = new Later(a _)
+  /**
+   * A type-safe & fast function composer.
+   *
+   * @see [[http://hackage.haskell.org/package/type-aligned]]
+   */
+  private trait AndThen[A, B] {
 
-  def always[A](a: => A): Eval[A] = new Always(a _)
+    /** Append a function to the last. O(1) */
+    def :+[C](f: B => Eval[C]): AndThen[A, C] = AndThen.append(this, f)
 
-  def defer[A](fa: => Eval[A]): Eval[A] = new Defer(fa _) {}
+    /** Concat a function list. O(1) */
+    def ++[C](r: AndThen[B, C]): AndThen[A, C] = AndThen.concat(this, r)
 
-  final private case class Now[A](value: A) extends Eval[A] { self =>
-    def memoize: Eval[A] = self
+    /** Extract the head and rest functions. Amortized O(1) */
+    def view: AndThenView[A, B] = AndThenView.from(this)
   }
 
-  final private class Always[A](f: () => A) extends Eval[A] {
-    def value: A = f()
+  private object AndThen {
+    def lift[A, B](f: A => Eval[B]): AndThen[A, B] = Leaf(f)
+    def empty[A]: AndThen[A, A] = Empty()
 
-    def memoize: Eval[A] = new Later(f)
+    def concat[A, B, C](l: AndThen[A, B], r: AndThen[B, C]): AndThen[A, C] =
+      (l, r) match {
+        case (_: Empty[A], r1) => r
+        case (l1, _: Empty[B]) => l
+        case _                 => Node(l, r)
+      }
+
+    def append[A, B, C](l: AndThen[A, B], f: B => Eval[C]): AndThen[A, C] = concat(l, Leaf(f))
+
+    final case class Empty[A]() extends AndThen[A, A]
+    final case class Leaf[A, B](f: A => Eval[B]) extends AndThen[A, B]
+    final case class Node[A, B, C](l: AndThen[A, B], r: AndThen[B, C]) extends AndThen[A, C]
   }
 
-  final private class Later[A](f: () => A) extends Eval[A] { self =>
-    private[this] var thunk: () => A = f
+  private trait AndThenView[A, B]
 
+  private object AndThenView {
+    import AndThen._
+
+    final case class VNil[A]() extends AndThenView[A, A]
+    final case class VCons[A, B, C](f: A => Eval[B], k: AndThen[B, C]) extends AndThenView[A, C]
+
+    def from[A, B](f: AndThen[A, B]): AndThenView[A, B] =
+      f match {
+        case _: Empty[A]   => VNil()
+        case l: Leaf[A, B] => VCons(l.f, AndThen.empty)
+        case Node(l, r) =>
+          @tailrec def loop[A1, B1, C1](l: AndThen[A1, B1], r: AndThen[B1, C1]): AndThenView[A1, C1] =
+            l match {
+              case _: Empty[A1]    => loop(r, AndThen.empty)
+              case l: Leaf[A1, B1] => VCons(l.f, r)
+              case Node(l1, r1)    => loop(l1, r1 ++ r)
+            }
+          loop(l, r)
+      }
+  }
+
+  final private case class Now[A](value: A) extends Eval[A] {
+    def memoize: Eval[A] = this
+  }
+
+  final private case class Always[A](thunk: () => A) extends Eval[A] {
+    def value: A = thunk()
+    def memoize: Eval[A] = Later(thunk)
+  }
+
+  final private case class Later[A](var thunk: () => A) extends Eval[A] {
     lazy val value: A = {
       val a = thunk()
       thunk = null
       a
     }
-
-    def memoize: Eval[A] = self
+    def memoize: Eval[A] = this
   }
 
-  abstract private class Defer[A](val thunk: () => Eval[A]) extends Eval[A] { self =>
-    def memoize: Eval[A] = Memoize(self)
-    def value: A = evaluate(self)
-  }
-
-  abstract private class FlatMap[A] extends Eval[A] { self =>
-    type S
-    val start: () => Eval[S]
-    val run: S => Eval[A]
-
-    def memoize: Eval[A] = Memoize(self)
-    def value: A = evaluate(self)
-  }
-
-  private case class Memoize[A](eval: Eval[A]) extends Eval[A] { self =>
-    var result: Option[A] = None
-    def memoize: Eval[A] = self
-    def value: A =
-      result match {
-        case Some(a) => a
-        case None =>
-          val a = evaluate(self)
-          result = Some(a)
-          a
-      }
-  }
-
-  @tailrec private def advance[A](fa: Eval[A]): Eval[A] =
-    fa match {
-      case call: Defer[A] =>
-        advance(call.thunk())
-      case compute: FlatMap[A] =>
-        new FlatMap[A] {
-          type S = compute.S
-          val start: () => Eval[S] = () => compute.start()
-          val run: S => Eval[A] = s => advance1(compute.run(s))
-        }
-      case other => other
+  final private case class Memoize[A](var fa: Eval[A]) extends Eval[A] {
+    var memo: Option[A] = None
+    def value: A = memo match {
+      case Some(a) => a
+      case None =>
+        val a = evaluate(this)
+        fa = null
+        memo = Some(a)
+        a
     }
+    def memoize: Eval[A] = this
+  }
 
-  private def advance1[A](fa: Eval[A]): Eval[A] = advance(fa)
+  final private case class Defer[A](fa: () => Eval[A]) extends Eval[A] {
+    val value: A = evaluate(this)
+    def memoize: Eval[A] = Memoize(this)
+  }
 
-  def evaluate[A](eval: Eval[A]): A = {
-    type L = Eval[Any]
-    type M = Memoize[Any]
-    type C = Any => Eval[Any]
+  final private case class FlatMap[A, B](fa: Eval[A], f: AndThen[A, B]) extends Eval[B] {
+    def value: B = evaluate(this)
+    def memoize: Eval[B] = Memoize(this)
+  }
 
-    def addToMemo(m: M): C = { a: Any =>
-      m.result = Some(a)
+  private def evaluate[A](fa: Eval[A]): A = {
+    import AndThenView._
+
+    def memo[A1](m: Memoize[A1]): AndThen[A1, A1] = AndThen.lift { a =>
+      m.memo = Some(a)
       Now(a)
     }
 
-    @tailrec def loop(curr: L, fs: List[C]): Any =
-      curr match {
-        case c: FlatMap[_] =>
-          c.start() match {
-            case cc: FlatMap[_] =>
-              loop(cc.start(), cc.run.asInstanceOf[C] :: c.run.asInstanceOf[C] :: fs)
-            case mm @ Memoize(eval) =>
-              mm.result match {
-                case Some(a) =>
-                  loop(Now(a), c.run.asInstanceOf[C] :: fs)
-                case None =>
-                  loop(eval, addToMemo(mm.asInstanceOf[M]) :: c.run.asInstanceOf[C] :: fs)
-              }
-            case xx => loop(c.run(xx.value), fs)
-          }
-        case call: Defer[_] =>
-          loop(advance(call), fs)
-        case m @ Memoize(eval) =>
-          m.result match {
+    @tailrec def loop[A1, B1](fa: Eval[A1], f: AndThen[A1, B1]): B1 =
+      fa match {
+        case FlatMap(fa1, f1) => loop(fa1, f1 ++ f)
+        case Defer(fa1)       => loop(fa1(), f)
+        case m: Memoize[A1] =>
+          m.memo match {
             case Some(a) =>
-              fs match {
-                case f :: fs => loop(f(a), fs)
-                case Nil     => a
+              f.view match {
+                case _: VNil[A1]  => a
+                case VCons(f1, k) => loop(f1(a), k)
               }
-            case None =>
-              loop(eval, addToMemo(m) :: fs)
+            case None => loop(m.fa, memo(m) ++ f)
           }
-        case x =>
-          fs match {
-            case f :: fs => loop(f(x.value), fs)
-            case Nil     => x.value
+        case _ =>
+          f.view match {
+            case _: VNil[A1]  => fa.value
+            case VCons(f1, k) => loop(f1(fa.value), k)
           }
       }
 
-    loop(eval.asInstanceOf[L], Nil).asInstanceOf[A]
-  }
-
-  implicit object EvalInstances extends Monad[Eval] {
-    def pure[A](a: A): Eval[A] = Eval.now(a)
-
-    override def map[A, B](fa: Eval[A])(f: A => B): Eval[B] = fa.map(f)
-
-    def flatMap[A, B](fa: Eval[A])(f: A => Eval[B]): Eval[B] = fa.flatMap(f)
+    loop(fa, AndThen.empty[A])
   }
 
   implicit def EvalEqInstances[A: Eq]: Eq[Eval[A]] = Eq[A].by(_.value)
+
+  implicit object EvalInstances extends Monad[Eval] {
+    def pure[A](a: A): Eval[A] = Now(a)
+    def flatMap[A, B](fa: Eval[A])(f: A => Eval[B]): Eval[B] = fa.flatMap(f)
+    override def map[A, B](fa: Eval[A])(f: A => B): Eval[B] = fa.map(f)
+  }
 }
