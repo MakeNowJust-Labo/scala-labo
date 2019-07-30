9338cef9b7f901e823c579748f4f25c2d8260e41
TSUYUSATO Kitsune <make.just.on@gmail.com>
2019-07-30 20:24:51 +0900

    fix: rename AndThen to FunTree

--- modules/neko/src/main/scala/codes/quine/labo/neko/data/Eval.scala
+++ modules/neko/src/main/scala/codes/quine/labo/neko/data/Eval.scala
@@ -4,7 +4,7 @@ package data
 
 import scala.annotation.tailrec
 
-trait Eval[A] {
+sealed trait Eval[A] {
   import Eval._
 
   def value: A
@@ -16,7 +16,7 @@ trait Eval[A] {
   def flatMap[B](f: A => Eval[B]): Eval[B] =
     this match {
       case FlatMap(fa, f1) => FlatMap(fa, f1 :+ f)
-      case _               => FlatMap(this, AndThen.lift(f))
+      case _               => FlatMap(this, FunTree.lift(f))
     }
 }
 
@@ -27,57 +27,62 @@ object Eval {
   def defer[A](fa: => Eval[A]): Eval[A] = Defer(fa _)
 
   /**
-   * A type-safe & fast function composer.
+   * A type-safe & fast A => Eval[B] function composer.
+   *
+   * It represents function compositions as binary tree.
    *
    * @see [[http://hackage.haskell.org/package/type-aligned]]
    */
-  private trait AndThen[A, B] {
-
+  private sealed trait FunTree[A, B] {
     /** Append a function to the last. O(1) */
-    def :+[C](f: B => Eval[C]): AndThen[A, C] = AndThen.append(this, f)
+    def :+[C](f: B => Eval[C]): FunTree[A, C] = FunTree.append(this, f)
 
     /** Concat a function list. O(1) */
-    def ++[C](r: AndThen[B, C]): AndThen[A, C] = AndThen.concat(this, r)
+    def ++[C](r: FunTree[B, C]): FunTree[A, C] = FunTree.concat(this, r)
 
     /** Extract the head and rest functions. Amortized O(1) */
-    def view: AndThenView[A, B] = AndThenView.from(this)
+    def view: FunList[A, B] = FunList.from(this)
   }
 
-  private object AndThen {
-    def lift[A, B](f: A => Eval[B]): AndThen[A, B] = Leaf(f)
-    def empty[A]: AndThen[A, A] = Empty()
+  private object FunTree {
+    /** An empty function composition (Eval.now: A => Eval[A]). */
+    final case class Empty[A]() extends FunTree[A, A]
+    /** A single A => Eval[B] function.  */
+    final case class Leaf[A, B](f: A => Eval[B]) extends FunTree[A, B]
+    /** A => Eval[B] and B => Eval[C] functions composition. */
+    final case class Node[A, B, C](l: FunTree[A, B], r: FunTree[B, C]) extends FunTree[A, C]
 
-    def concat[A, B, C](l: AndThen[A, B], r: AndThen[B, C]): AndThen[A, C] =
+    def lift[A, B](f: A => Eval[B]): FunTree[A, B] = Leaf(f)
+    def empty[A]: FunTree[A, A] = Empty()
+
+    def concat[A, B, C](l: FunTree[A, B], r: FunTree[B, C]): FunTree[A, C] =
       (l, r) match {
-        case (_: Empty[A], r1) => r
-        case (l1, _: Empty[B]) => l
+        case (_: Empty[A], _) => r
+        case (_, _: Empty[B]) => l
         case _                 => Node(l, r)
       }
 
-    def append[A, B, C](l: AndThen[A, B], f: B => Eval[C]): AndThen[A, C] = concat(l, Leaf(f))
-
-    final case class Empty[A]() extends AndThen[A, A]
-    final case class Leaf[A, B](f: A => Eval[B]) extends AndThen[A, B]
-    final case class Node[A, B, C](l: AndThen[A, B], r: AndThen[B, C]) extends AndThen[A, C]
+    def append[A, B, C](l: FunTree[A, B], f: B => Eval[C]): FunTree[A, C] = concat(l, Leaf(f))
   }
 
-  private trait AndThenView[A, B]
+  /** [[FunTree]] view. */
+  private sealed trait FunList[A, B]
 
-  private object AndThenView {
-    import AndThen._
+  private object FunList {
+    import FunTree._
 
-    final case class VNil[A]() extends AndThenView[A, A]
-    final case class VCons[A, B, C](f: A => Eval[B], k: AndThen[B, C]) extends AndThenView[A, C]
+    final case class FLNil[A]() extends FunList[A, A]
+    final case class FLCons[A, B, C](f: A => Eval[B], k: FunTree[B, C]) extends FunList[A, C]
 
-    def from[A, B](f: AndThen[A, B]): AndThenView[A, B] =
+    def from[A, B](f: FunTree[A, B]): FunList[A, B] =
       f match {
-        case _: Empty[A]   => VNil()
-        case l: Leaf[A, B] => VCons(l.f, AndThen.empty)
+        case _: Empty[A]   => FLNil()
+        case l: Leaf[A, B] => FLCons(l.f, FunTree.empty)
         case Node(l, r) =>
-          @tailrec def loop[A1, B1, C1](l: AndThen[A1, B1], r: AndThen[B1, C1]): AndThenView[A1, C1] =
+          @tailrec def loop[A1, B1, C1](l: FunTree[A1, B1], r: FunTree[B1, C1]): FunList[A1, C1] =
             l match {
-              case _: Empty[A1]    => loop(r, AndThen.empty)
-              case l: Leaf[A1, B1] => VCons(l.f, r)
+              case _: Empty[A1]    => loop(r, FunTree.empty)
+              case l: Leaf[A1, B1] => FLCons(l.f, r)
               case Node(l1, r1)    => loop(l1, r1 ++ r)
             }
           loop(l, r)
@@ -113,20 +118,20 @@ object Eval {
     def memoize: Eval[A] = Memoize(this)
   }
 
-  final private case class FlatMap[A, B](fa: Eval[A], f: AndThen[A, B]) extends Eval[B] {
+  final private case class FlatMap[A, B](fa: Eval[A], f: FunTree[A, B]) extends Eval[B] {
     def value: B = evaluate(this)
     def memoize: Eval[B] = Memoize(this)
   }
 
   private def evaluate[A](fa: Eval[A]): A = {
-    import AndThenView._
+    import FunList._
 
-    def memo[A1](m: Memoize[A1]): AndThen[A1, A1] = AndThen.lift { a =>
+    def memo[A1](m: Memoize[A1]): FunTree[A1, A1] = FunTree.lift { a =>
       m.memo = Some(a)
       Now(a)
     }
 
-    @tailrec def loop[A1, B1](fa: Eval[A1], f: AndThen[A1, B1]): B1 =
+    @tailrec def loop[A1, B1](fa: Eval[A1], f: FunTree[A1, B1]): B1 =
       fa match {
         case FlatMap(fa1, f1) => loop(fa1, f1 ++ f)
         case Defer(t)       => loop(t(), f)
@@ -134,19 +139,19 @@ object Eval {
           m.memo match {
             case Some(a) =>
               f.view match {
-                case _: VNil[A1]  => a
-                case VCons(f1, k) => loop(f1(a), k)
+                case _: FLNil[A1]  => a
+                case FLCons(f1, k) => loop(f1(a), k)
               }
             case None => loop(m.fa, memo(m) ++ f)
           }
         case _ =>
           f.view match {
-            case _: VNil[A1]  => fa.value
-            case VCons(f1, k) => loop(f1(fa.value), k)
+            case _: FLNil[A1]  => fa.value
+            case FLCons(f1, k) => loop(f1(fa.value), k)
           }
       }
 
-    loop(fa, AndThen.empty[A])
+    loop(fa, FunTree.empty[A])
   }
 
   implicit def EvalEqInstances[A: Eq]: Eq[Eval[A]] = Eq[A].by(_.value)
