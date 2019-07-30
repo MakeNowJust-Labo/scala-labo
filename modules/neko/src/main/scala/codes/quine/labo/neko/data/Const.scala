7e6645dc9be43969bfe556579db26279b8e24311
TSUYUSATO Kitsune <make.just.on@gmail.com>
2019-07-30 20:33:56 +0900

    feat: add Const

---
+++ modules/neko/src/main/scala/codes/quine/labo/neko/data/Const.scala
@@ -0,0 +1,10 @@
+package codes.quine.labo.neko
+package data
+
+final case class Const[A, B](value: A)
+
+object Const {
+  def ConstInstances[A]: Functor[Const[A, *]] = new Functor[Const[A, *]] {
+    def map[B, C](fa: Const[A, B])(f: B => C): Const[A, C] = Const(fa.value)
+  }
+}
