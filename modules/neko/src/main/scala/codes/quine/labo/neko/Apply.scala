69b138342c86dcaecdb86c837c5a1f6ca5f4c367
TSUYUSATO Kitsune <make.just.on@gmail.com>
2019-08-07 02:11:10 +0900

    feat: add Apply#map2

--- modules/neko/src/main/scala/codes/quine/labo/neko/Apply.scala
+++ modules/neko/src/main/scala/codes/quine/labo/neko/Apply.scala
@@ -6,4 +6,7 @@ import simulacrum.typeclass
 @typeclass trait Apply[F[_]] extends Functor[F] {
   @simulacrum.op("<*>", alias = true)
   def ap[A, B](ff: F[A => B])(fa: F[A]): F[B]
+
+  def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] =
+    ap(map(fa)(a => (b: B) => f(a, b)))(fb)
 }
